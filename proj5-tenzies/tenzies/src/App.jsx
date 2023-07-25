a4373006659a42c2a37e76ce4c796056241083f9
Seunghan Lee <shlee4458@gmail.com>
2023-07-25 18:43:21 -0400

    refactor

--- proj5-tenzies/tenzies/src/App.jsx
+++ proj5-tenzies/tenzies/src/App.jsx
@@ -1,23 +1,44 @@
 import React from "react"
-import Main from "./components/Main.jsx"
-
+import Dice from "./components/Dice.jsx"
+import {nanoid} from "nanoid"
 
 
 export default function App() {
-  function clickHandler() {
-    console.log("clicked")
+  function rollDice() {
+    setNumArray(getRandom())
   }
 
   // create an array that holds 10 random numbers
-  const numArray = [1,2,3,4,5,6,7,8,9,10]
+  function getRandom() {
+    let numArray = [];
+    for (let i = 0; i < 10; i++) {
+      let randNum = Math.floor(Math.random() * 6) + 1;
+      numArray.push({
+        value: randNum,
+        isHeld: false,
+        id: nanoid()
+      })
+    }
+    return numArray
+  }
+
+  const [numArray, setNumArray] = React.useState(getRandom())
+  const diceElements = numArray.map((die) => {
+    return <Dice key={die.id} value={die.value}/>
+  })
 
   return (
-    <div>
-      <Main
-      numArray={numArray}
-      clickHandler={clickHandler}
-      />
-    </div>
+    <main className="main">
+      <div className="main__inner">
+        <p className="main__title">Tenzies</p>
+        <p className="main__instruction">Roll until all dice are the same. 
+        Click each die to freeze it at its current value between rolls.</p>
+        <div className="dice">
+          {diceElements}
+        </div>
+        <button className="main__button" onMouseEnter={rollDice}>Roll</button>
+      </div>
+    </main>
   )
 }
 
---
+++ proj5-tenzies/tenzies/src/components/Dice.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Dice(props) {
+    return (
+    <div className="dice__number">{props.value}</div>
+    )
+}
