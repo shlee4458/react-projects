82fed0f9501eea8fc71b94f890abd08ada0c797a
Seunghan Lee <shlee4458@gmail.com>
2023-07-26 18:35:25 -0400

    add winning logic and effect

--- proj5-tenzies/tenzies/src/App.jsx
+++ proj5-tenzies/tenzies/src/App.jsx
@@ -1,34 +1,91 @@
 import React from "react"
 import Dice from "./components/Dice.jsx"
 import {nanoid} from "nanoid"
-
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 
 export default function App() {
+
+  const [numArray, setNumArray] = React.useState(getRandom())
+  const [tenzies, setTenzies] = React.useState(false);
+  const { width, height } = useWindowSize();
+
+  // checks if the user has won the game every time the numArray changes
+  React.useEffect(() => {
+    if (wonGame()) {
+      setTenzies(true)
+      console.log("won the gmae")
+    }
+  }, [numArray])
+
+  // check if the user has won the game
+  function wonGame() {
+
+    // using filter
+    // let firstValue = numArray[0].value
+    // numArray.filter((die) => {
+    //   die.isHeld && die.value == value
+    // })
+
+    // return numArray.length == 10
+
+    // using every
+    const isAllHeld = numArray.every(die => die.isHeld)
+    const firstValue = numArray[0].value
+    const allTheSame = numArray.every(die => die.value = firstValue)
+    return isAllHeld && allTheSame
+  }
+
+  const diceElements = numArray.map((die) => {
+    return <Dice isHeld={die.isHeld} key={die.id} value={die.value} holdDice={() => holdDice(die.id)}/>
+  })
+
+  // set the num array with the new` random array
   function rollDice() {
-    setNumArray(getRandom())
+    if (!tenzies) {
+      setNumArray(oldNumArray => oldNumArray.map(die => {
+        return die.isHeld ? die : generateNewDie() 
+      }))
+    } else {
+      setNumArray(getRandom()) // if user restarts the game
+      setTenzies(false)
+    }
   }
 
   // create an array that holds 10 random numbers
   function getRandom() {
     let numArray = [];
     for (let i = 0; i < 10; i++) {
-      let randNum = Math.floor(Math.random() * 6) + 1;
-      numArray.push({
-        value: randNum,
-        isHeld: false,
-        id: nanoid()
-      })
+      numArray.push(generateNewDie())
     }
     return numArray
   }
 
-  const [numArray, setNumArray] = React.useState(getRandom())
-  const diceElements = numArray.map((die) => {
-    return <Dice key={die.id} value={die.value}/>
-  })
+  // randomly generate a single die
+  function generateNewDie() {
+    return {
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
+  // clickedHandler function 
+  // when the user clicks the dice, updates the isHeld to !isHeld
+    
+  function holdDice(id) {
+    setNumArray(oldNumArray => {
+      return oldNumArray.map(die => {
+        return die.id === id ? {
+          ...die, isHeld: !die.isHeld
+        } : die
+      })
+    })
+  }
 
   return (
     <main className="main">
+      {tenzies ? <Confetti/> : ""}
       <div className="main__inner">
         <p className="main__title">Tenzies</p>
         <p className="main__instruction">Roll until all dice are the same. 
@@ -36,7 +93,7 @@ export default function App() {
         <div className="dice">
           {diceElements}
         </div>
-        <button className="main__button" onMouseEnter={rollDice}>Roll</button>
+        <button className="main__button" onMouseEnter={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
       </div>
     </main>
   )
--- proj5-tenzies/tenzies/src/components/Dice.jsx
+++ proj5-tenzies/tenzies/src/components/Dice.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 export default function Dice(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+
     return (
-    <div className="dice__number">{props.value}</div>
+    <div className={"dice__number"} style={styles} id={props.id} onClick={props.holdDice}>{props.value}</div>
     )
 }
